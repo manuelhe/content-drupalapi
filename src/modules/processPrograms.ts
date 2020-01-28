1cae3bf67bb3f0cc2b72f5d9d92c3911b4319ef7
Manuel Herrera <334509+manuelhe@users.noreply.github.com>
2020-01-28 00:22:59 -0500

    Visual enhancement of output

--- src/modules/processPrograms.ts
+++ src/modules/processPrograms.ts
@@ -6,17 +6,36 @@ import postProgram from "./postProgram";
 const { contentSpreadsheetId, contentSpreadsheetCredentials } = Config;
 
 export default async (isDryRun: boolean) => {
+  const profileInfo = {
+    startTime: Date.now(),
+    totalProgramsCreated: 0,
+  };
+
   // Fetch data
   const data = await fetchSheet(contentSpreadsheetId, "programs", contentSpreadsheetCredentials);
 
-  console.log(`Starting processing ${data.length} programs`);
+  console.log("\x1b[36m%s\x1b[0m", `Start processing ${data.length} programs\n`);
 
   for (const item of data) {
     console.info(`Posting program: ${slugify(item.code)}`);
     if (!isDryRun) {
       const response = await postProgram(item);
-
-      console.log(`Program ${response} created succesfully`);
+      break;
+      if (!response) {
+        console.log("\x1b[31m%s\x1b[0m", "Error creating program");
+        continue;
+      }
+      console.log("\x1b[32m%s\x1b[0m", `Program ${response.id} created succesfully`);
+      profileInfo.totalProgramsCreated++;
     }
   }
+
+  // Profiling info
+  const date = new Date(Date.now() - profileInfo.startTime);
+  const h = date.getUTCHours();
+  const m = date.getUTCMinutes();
+  const s = date.getUTCSeconds();
+
+  console.log("\n\x1b[42m%s\x1b[0m", `Total programs created: ${profileInfo.totalProgramsCreated}`);
+  console.log("\x1b[36m%s\x1b[0m", `Time spent: ${((h * 60) + m)} mins ${s} secs`);
 };
